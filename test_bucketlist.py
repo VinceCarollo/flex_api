49688ec4298ddb13f8d4c2a24840f30b4228a6a9
Vince Carollo <vincecarollo@gmail.com>
2019-07-17 21:07:59 -0600

    Add virtual environment

--- test_bucketlist.py
+++ test_bucketlist.py
@@ -1,15 +1,19 @@
 import unittest
 import os
 import json
-from IPython import embed
+from app import app
 
 class BucketlistTestCase(unittest.TestCase):
 
      def setUp(self):
-         self.app = create_app(config_name="testing")
+         self.app = app
          self.client = self.app.test_client
          self.buckelist = {'name' : 'dfd'}
 
-     def test_food_info(arg):
-        res = self.client().post('/food_info/cheeseburger?size=small')
-        embed()
+     def test_food_info(self):
+        res = self.client().post('/food_info/cheeseburger/?size=small')
+        import code; code.interact(local=dict(globals(), **locals()))
+        self.assertEqual(res.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
